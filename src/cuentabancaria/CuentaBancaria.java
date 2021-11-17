c0cde3fad5d29e4b8dd82fdf22736e9f7d7ad36c
miria <miria@LAPTOP-I717VUH0>
2021-11-17 18:20:27 +0100

    Commit 1

---
+++ src/cuentabancaria/CuentaBancaria.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentabancaria;
+
+/**
+ *
+ * @author miria
+ */
+public class CuentaBancaria {
+
+    private int id, balance;
+   
+    
+    public CuentaBancaria(int id, int balance){
+        
+        this.id = id;
+        this.balance = balance;
+        
+    }
+    
+    
+   public void Withdraw(int x) throws Exception{
+       
+       
+       
+      
+   }
+    
+   public void Deposit(int x){
+       
+       
+   }
+   
+   public int getBalance(){
+       return 0;
+   }
+}
